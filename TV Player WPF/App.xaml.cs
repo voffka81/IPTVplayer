7e04b83728eb3706cae2640e476036071b4a9abc
Vova <3emaster@gmail.com>
2024-01-28 20:19:49 +0200

    show general error

--- TV Player WPF/App.xaml.cs
+++ TV Player WPF/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Controls.Primitives;
 using TV_Player.ViewModels;
 
 namespace TV_Player
@@ -9,13 +10,32 @@ namespace TV_Player
     public partial class App : Application
     {
         private TVPlayerViewModel _tvPlayer;
+       
+        private const string Guid = "250C5597-BA73-40DF-B2CF-DD644F044834";
+        static readonly Mutex Mutex = new Mutex(true, "{" + Guid + "}");
         protected override void OnStartup(StartupEventArgs e)
         {
+            AppDomain.CurrentDomain.UnhandledException += ReportAndRestart;
+            
+            if (!Mutex.WaitOne(TimeSpan.Zero, true))
+            {
+                MessageBox.Show("Programm already running");
+                Current.Shutdown();
+            }
+
             _tvPlayer = new TVPlayerViewModel();
 
             base.OnStartup(e);
         }
 
+        protected void ReportAndRestart(object sender, UnhandledExceptionEventArgs e)
+        {
+
+            string info = e.ExceptionObject.ToString();
+            MessageBox.Show(info,"Application");
+            
+            Environment.Exit(1);
+        }
         protected override void OnExit(ExitEventArgs e)
         {
             _tvPlayer = null;
